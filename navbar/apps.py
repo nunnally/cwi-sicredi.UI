6c292ed1ca45a048423fa422513b85e48da59d2c
César Augusto <cesarisadog@gmail.com>
2022-10-14 10:37:51 -0300

    navbar ins't a good name?

--- navbar/apps.py
+++ navbar/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HeaderConfig(AppConfig):
+class NavbarConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'header'
+    name = 'navbar'
--- sicredi/settings.py
+++ sicredi/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base',
     'menu',
-    'header',
+    'navbar',
     'content',
     
 ]
